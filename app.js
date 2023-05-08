ebbe1b9cb8695704e8efb5bf85ca9fb481b7f9a9
MINSU <minsu@MINSUui-MacBookPro.local>
2023-05-08 23:48:29 +0900

    Canvas Paths

--- app.js
+++ app.js
@@ -8,4 +8,14 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 700;
 
-ctx.fillRect(5, 5, 300, 300);
+ctx.rect(50, 50, 200, 200); //사각형을 만든다
+ctx.rect(250, 250, 200, 200); //사각형을 만든다
+ctx.fill();
+//도형을 만든 후 fill할지 stroke할지 고르게 된다
+//fill -> 색을 채워 넣는 것 stroke -> 선만 있는 것
+ctx.beginPath(); //위에 있는 사각형과 다른 경로를 만든다.
+ctx.rect(450, 450, 200, 200); //사각형을 만든다
+ctx.fillStyle = 'green';
+ctx.fill(); //위에 있는 사각형을 단색으로 채운다
+
+//모든 사각형의 색이 변하는 이유는 같은 경로에 있기 때문이다.
